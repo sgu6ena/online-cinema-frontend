b6061d01b7fab5bb42471290b10f50aa01f50838
sgu6ena <sgu6ena@gmail.com>
2022-04-06 21:36:08 +0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	poweredByHeader: false,
+	optimizeFonts: false,
+	env: {
+		APP_URL: process.env.REACT_APP_URL,
+		APP_ENV: process.env.REACT_APP_ENV,
+		APP_SERVER_URL: process.env.REACT_SERVER_APP_URL,
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/path*',
+				destination: 'http://localhost:4400/api/:path',
+			},
+			{
+				source: '/uploads/path*',
+				destination: 'http://localhost:4400/uploads/:path',
+			},
+		]
+	},
 }
 
 module.exports = nextConfig
