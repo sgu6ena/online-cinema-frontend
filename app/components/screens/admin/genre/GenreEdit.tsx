7732be2e6dde228402c374dd27851963e50adcd5
sgu6ena <issar@admin.idknet.com>
2022-04-19 12:59:27 +0300

    initial genre edit

--- app/components/screens/admin/genre/GenreEdit.tsx
+++ app/components/screens/admin/genre/GenreEdit.tsx
@@ -1,72 +1,72 @@
-import { FC } from 'react'
-import { useForm } from 'react-hook-form'
+import {FC} from 'react'
+import {useForm} from 'react-hook-form'
 
 import Meta from '../../../../utils/meta/Meta'
+import {generateSlug} from '../../../../utils/string/generateSlug'
 import SkeletonLoader from '../../../ui/SkeletonLoader'
 import AdminNavigation from '../../../ui/admin-navigation/AdminNavigation'
+import Button from '../../../ui/form-elemets/Button'
 import Field from '../../../ui/form-elemets/Field'
+import styles from '../../../ui/form-elemets/admin-form.module.scss'
+import SlugField from '../../../ui/form-elemets/slugField/SlugField'
 import Heading from '../../../ui/heading/Heading'
 
-import { IGenreEditInput } from './genre-edit.interface'
-import { useGenreEdit } from './useGenreEdit'
-
-import styles from 'GenreEdit.module.scss'
+import {IGenreEditInput} from './genre-edit.interface'
+import {useGenreEdit} from './useGenreEdit'
 
 const GenreEdit: FC = () => {
 	const {
 		handleSubmit,
 		register,
-		formState: { errors },
+		formState: {errors},
 		setValue,
 		getValues,
 	} = useForm<IGenreEditInput>({
 		mode: 'onChange',
 	})
 
-	const { isLoading, onSubmit } = useGenreEdit(setValue)
+	const {isLoading, onSubmit} = useGenreEdit(setValue)
 	return (
-		<Meta title="Редактирование жанра">
-			<AdminNavigation />
-			<Heading title={'Редактирование жанра'} />
-			<form onSubmit={handleSubmit(onSubmit)}>
-				{isLoading ? (
-					<SkeletonLoader count={3} />
-				) : (
-					<>
-						<div>
-							<Field
-								{...register('name', {
-									required: 'Название жанра обязательно!',
-								})}
-								placeholder="Название жанра"
-								error={errors.name}
-								style={{ width: '31%' }}
-							/>
-							<Field
-								{...register('icon', {
-									required: 'Иконка для жанра обязательна!',
-								})}
-								placeholder="Иконка для жанра"
-								error={errors.icon}
-								style={{ width: '31%' }}
-							/>
-							<Field
-								{...register('slug', {
-									required: 'URL для жанра обязательно!',
-								})}
-								placeholder="URL для жанра"
-								error={errors.slug}
-								style={{ width: '31%' }}
-							/>
+			<Meta title="Редактирование жанра">
+				<AdminNavigation/>
+				<Heading title={'Редактирование жанра'}/>
+				<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+					{isLoading ? (
+							<SkeletonLoader count={3}/>
+					) : (
+							<>
+								<div className={styles.fields}>
+									<Field
+											{...register('name', {
+												required: 'Название жанра обязательно!',
+											})}
+											placeholder="Название жанра"
+											error={errors.name}
+											style={{width: '31%'}}
+									/> <SlugField
+										generate={() => {
+											setValue('slug', generateSlug(getValues('name')))
+										}}
+										register={register}
+										error={errors.slug}
 
-							{/*	Text editor*/}
+								/>
+									<Field
+											{...register('icon', {
+												required: 'Иконка для жанра обязательна!',
+											})}
+											placeholder="Иконка для жанра"
+											error={errors.icon}
+											style={{width: '31%'}}
+									/>
 
-							<button>Обновить</button>
-						</div>
-					</>
-				)}
-			</form>
-		</Meta>
+									{/*	Text editor*/}
+									<Button>Обновить</Button>
+								</div>
+							</>
+					)}
+				</form>
+			</Meta>
 	)
 }
 
