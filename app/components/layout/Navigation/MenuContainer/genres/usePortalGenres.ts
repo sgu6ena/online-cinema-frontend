a21afc2ebabecf678b22567be6ad596f38dd9721
sgu6ena <issar@admin.idknet.com>
2022-05-18 10:05:06 +0300

    cleaning

--- app/components/layout/Navigation/MenuContainer/genres/usePortalGenres.ts
+++ app/components/layout/Navigation/MenuContainer/genres/usePortalGenres.ts
@@ -1,30 +1,31 @@
-import { useQuery } from "react-query"
-import { getGenreUrl } from "../../../../../config/url.config"
-import { GenreService } from "../../../../../services/genre.service"
-import { IMenuItem } from "../menu.interface"
+import {useQuery} from "react-query"
+import {getGenreUrl} from "../../../../../config/url.config"
 
-export const usePortalGenres=()=>{
-	const queryData = useQuery(
-		'popular genre menu',
-		() => GenreService.getPortal(),
-		{
-			select: ({ data }) =>
-				data.filter((genre: { type: number })=>genre.type===3)
-					.map(
-						(genre:any) =>
-							({
-								title: genre.title,
-								link: getGenreUrl(genre.cid),
-								icon: 'MdApi',
-							} as IMenuItem)
-					),
+import {IMenuItem} from "../menu.interface"
+import {GenreService} from "../../../../../services/genre.service";
 
-			onError(error) {
-				//TODO: errors
-			},
-		}
-	)
+export const usePortalGenres = () => {
+    const queryData = useQuery(
+        'popular genre menu',
+        () => GenreService.getPortal(),
+        {
+            select: ({data}) =>
+                data.filter((genre: { type: number }) => genre.type === 3)
+                    .map(
+                        (genre: any) =>
+                            ({
+                                title: genre.title,
+                                link: getGenreUrl(genre.cid),
+                                icon: 'MdApi',
+                            } as IMenuItem)
+                    ),
 
-	return queryData
+            onError(error) {
+                //TODO: errors
+            },
+        }
+    )
+
+    return queryData
 
 }
--- app/services/admin.service.ts
+++
@@ -1,8 +0,0 @@
-import axios from "../api/interceptors"
-import { getUserUrl } from "../config/api.config"
-
-export const AdminService={
-	async getCountUsers(){
-		return axios.get<number>(getUserUrl('/count'))
-	}
-}
