cfbb4fbb203b12823b3ea2640ec5158e316c93a7
sgu6ena <issar@admin.idknet.com>
2022-05-18 15:41:45 +0300

    movie page + skeleton

---
+++ app/components/screens/movie/MovieSkeleton.tsx
@@ -0,0 +1,29 @@
+import React, {FC} from 'react';
+import SkeletonLoader from "../../ui/SkeletonLoader";
+import styles from './Movie.module.scss'
+
+const MovieSkeleton: FC = () => {
+    return (
+        <div className='pl-layout pt-layout flex justify-between'>
+            <div className='w-3/4 mr-5'>
+                <div className='w-3/4 '>
+                    <div className='w-60'>
+                    <SkeletonLoader count={1} className={'h-12 w-12 '}/>
+                    </div>
+                    <div className='w-full'>
+                        <SkeletonLoader count={1} className={'h-5 mt-10'}/>
+                    </div>
+                    <div className='w-96'>
+                        <SkeletonLoader count={1} className={'h-5 mt-10'}/>
+                    </div>
+
+                </div><SkeletonLoader count={5} className={'h-5 mt-6'}/>
+            </div>
+            <div className='w-1/4 '>
+                <SkeletonLoader count={1} className={'h-96'}/>
+            </div>
+        </div>
+    );
+};
+
+export default MovieSkeleton;
--- app/components/screens/movie/Season.tsx
+++ app/components/screens/movie/Season.tsx
@@ -1,31 +1,41 @@
 import cn from 'classnames'
-import { FC } from 'react'
+import {FC, useEffect, useState} from 'react'
 
-import { IMedia } from '../../../shared/types/movie.types'
+import {IMedia} from '../../../shared/types/movie.types'
 
 import styles from './Movie.module.scss'
+import Subheading from "../../ui/heading/Subheading";
 
 export interface ISeason extends IMedia {
-	index: number
-	fn: (id: number) => void
+    index: number
+    fn: (id: number) => void
 }
 
-const Season: FC<ISeason> = ({ isActive = false, items, fn }) => {
-	return (
-		<div className={styles.season}>
-			<div className={cn(!isActive ? 'hidden' : styles.active)}>
-				{items.map((item) => (
-					<button
-						key={item.file}
-						className={styles.episode}
-						onClick={() => fn(item.file)}
-					>
-						{item.title}
-					</button>
-				))}
-			</div>
-		</div>
-	)
+const Season: FC<ISeason> = ({isActive = false, items, fn}) => {
+
+
+    return (
+        <div className={styles.season}>
+            <div className={cn(!isActive ? 'hidden' : styles.active)}>
+                {items.map((item) => (
+                    <>
+                        <button
+                            key={item.file}
+                            className={styles.episode}
+                            onClick={() => {
+                                return fn(item.file)
+                            }}
+                        >
+                            {item.title}
+                        </button>
+
+                    </>
+                ))}
+            </div>
+        </div>
+
+
+    )
 }
 
 export default Season
