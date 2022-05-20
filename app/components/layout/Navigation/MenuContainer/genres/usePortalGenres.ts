47786f91321126164aa7e2705c86a32dd41755ee
sgu6ena <issar@admin.idknet.com>
2022-05-20 17:21:57 +0300

    genres

--- app/components/layout/Navigation/MenuContainer/genres/usePortalGenres.ts
+++ app/components/layout/Navigation/MenuContainer/genres/usePortalGenres.ts
@@ -1,27 +1,27 @@
-import {useQuery} from "react-query"
-import {getGenreUrl} from "../../../../../config/url.config"
+import { useQuery } from 'react-query'
+import { getGenreUrl } from '../../../../../config/url.config'
 
-import {IMenuItem} from "../menu.interface"
-import {GenreService} from "../../../../../services/genre.service";
+import { IMenuItem } from '../menu.interface'
+import { GenreService } from '../../../../../services/genre.service'
 
 export const usePortalGenres = () => {
-    const queryData = useQuery(
-        'popular genre menu',
-        () => GenreService.getPortal(),
-        {
-            select: ({data}) =>
-                data.filter((genre: { type: number }) => genre.type === 3).sort((i, j) => (parseInt(i.cid) - parseInt(j.cid)))
-                    .map(
-                        (genre: any) =>
-                            ({
-                                title: genre.title,
-                                link: getGenreUrl(genre.cid),
-                                icon: 'MdApi',
-                            } as IMenuItem)
-                    ),
-        }
-    )
+	const queryData = useQuery(
+		'popular genre menu',
+		() => GenreService.getPortal(),
+		{
+			select: ({ data }) =>
+				data.filter((genre: { type: number }) => genre.type === 3).sort((i, j) => (parseInt(i.cid) - parseInt(j.cid)))
+					.map(
+						(genre: any) =>
+							({
+								title: genre.title,
+								link: getGenreUrl(genre.cid),
+								icon: 'MdApi',
+							} as IMenuItem),
+					),
+		},
+	)
 
-    return queryData
+	return queryData
 
 }
