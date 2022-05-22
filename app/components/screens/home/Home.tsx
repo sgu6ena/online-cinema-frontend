de2ff7571bc4845702d2ab333deaf0fe5edf47ca
sgu6ena <sgu6ena@gmail.com>
2022-05-22 18:26:19 +0300

    home mobile ADAPTIVE

--- app/components/screens/home/Home.tsx
+++ app/components/screens/home/Home.tsx
@@ -1,59 +1,61 @@
 import Link from 'next/link'
-import { FC } from 'react'
+import {FC} from 'react'
 
-import { getMoviesUrl } from '../../../config/api.config'
-import { getGenreUrl } from '../../../config/url.config'
-import { IMoviePortal } from '../../../shared/types/movie.types'
+import MaterialIcon from '../../ui/MaterialIcon'
+
+import {getMoviesUrl} from '../../../config/api.config'
+import {getGenreUrl} from '../../../config/url.config'
+import {IMoviePortal} from '../../../shared/types/movie.types'
 import Meta from '../../../utils/meta/Meta'
 import Gallery from '../../ui/gallery/Gallery'
-import { IGalleryItem } from '../../ui/gallery/gallery.interface'
+import {IGalleryItem} from '../../ui/gallery/gallery.interface'
 import Subheading from '../../ui/heading/Subheading'
 import Slider from '../../ui/slider/Slider'
 
-import { IHome } from './home.interface'
-import { usePortalSlides } from './usePortalSlides'
 import HomeLoading from './HomeLoading'
+import {IHome} from './home.interface'
+import {usePortalSlides} from './usePortalSlides'
 
 const collectionsToItems = (items: IMoviePortal[]): IGalleryItem[] => {
-	return [
-		...items.map((i) => ({
-			link: getMoviesUrl(i.id),
-			name: i.title,
-			posterPath: i.logo,
-		})),
-	]
+    return [
+        ...items.map((i) => ({
+            link: getMoviesUrl(i.id),
+            name: i.title,
+            posterPath: i.logo,
+        })),
+    ]
 }
 const Home: FC<IHome> = () => {
+    const {isLoading, slides, collections} = usePortalSlides()
 
-	const { isLoading, slides, collections } = usePortalSlides()
+    return (
+        <>
+            <Meta
+                title="PORTAL"
+                description="Фильмы на любой вкус, мультфильмы, популярные сериалы, новинки от ведущих мировых киностудий"
+            ></Meta>
+            {isLoading && <HomeLoading/>}
+            {!isLoading && slides && <Slider slides={slides}/>}
 
-	return (
-		<>
-			<Meta
-				title='PORTAL'
-				description='Фильмы на любой вкус, мультфильмы, популярные сериалы, новинки от ведущих мировых киностудий'
-			></Meta>
-			{isLoading && <HomeLoading />}
-			{!isLoading && slides && <Slider slides={slides} />}
-
-			{collections &&
-				collections.map((c) => (
-					<div className='my-10 px-layout ' key={c.cid}>
-						<div className={'flex-center-between'}>
-							<Subheading title={c.title} />
-							<Link href={getGenreUrl(c.cid.toString())}>
-								<a>
-									<button className='btn-primary px-5 py-2 mb-5'>
-										Смотреть все
-									</button>
-								</a>
-							</Link>
-						</div>
-						<Gallery items={collectionsToItems(c.items)} />
-					</div>
-				))}
-		</>
-	)
+            {collections &&
+                collections.map((c) => (
+                    <div className="my-10 px-2 lg:px-layout " key={c.cid}>
+                        <div className={'flex-center-between'}>
+                            <Subheading title={c.title}/>
+                            <Link href={getGenreUrl(c.cid.toString())}>
+                                <a>
+                                    <button className="btn-primary flex-center-between  lg:px-5 px-2 py-2 mb-5">
+                                        <span className="hidden lg:inline"> Смотреть все</span>
+                                        <MaterialIcon name="MdChevronRight"/>
+                                    </button>
+                                </a>
+                            </Link>
+                        </div>
+                        <Gallery items={collectionsToItems(c.items)}/>
+                    </div>
+                ))}
+        </>
+    )
 }
 
 export default Home
