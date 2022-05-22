52381c25ab00b0939b76671281a7568c03baabe4
sgu6ena <sgu6ena@gmail.com>
2022-05-22 13:13:21 +0300

    movie mobile adaptive

--- app/components/screens/movie/Movie.tsx
+++ app/components/screens/movie/Movie.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 
-import { FC, useEffect, useState } from 'react'
+import {FC, useEffect, useState} from 'react'
 
 
-import { PortalMovieService } from '../../../services/portalMovie.service'
+import {PortalMovieService} from '../../../services/portalMovie.service'
 
 import FilmTag from '../../ui/FilmTag'
 
@@ -12,9 +12,9 @@ import Vote from '../../ui/vote/Vote'
 import styles from './Movie.module.scss'
 import Rating from './Rating'
 import Tabs from './Tabs'
-import { usePortalMovie } from './usePortalMovie'
+import {usePortalMovie} from './usePortalMovie'
 
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router'
 
 
 import VideoPLayer from '../../ui/videoPlayer/VideoPLayer'
@@ -23,73 +23,73 @@ import MovieSkeleton from './MovieSkeleton'
 
 
 const Movie: FC = () => {
-	const { asPath } = useRouter()
-	const { isLoading, movie } = usePortalMovie()
-	const [idFile, setIdFile] = useState('')
-	const [url, setUrl] = useState('')
-	const [play, setPlay] = useState(false)
+    const {asPath} = useRouter()
+    const {isLoading, movie} = usePortalMovie()
+    const [idFile, setIdFile] = useState('')
+    const [url, setUrl] = useState('')
+    const [play, setPlay] = useState(false)
 
-	const handleMovie = (id: number) => {
-		setIdFile(id.toString())
-		setPlay(true)
-	}
+    const handleMovie = (id: number) => {
+        setIdFile(id.toString())
+        setPlay(true)
+    }
 
-	useEffect(() => {
-		setUrl('')
-		setPlay(false)
-		setIdFile('')
-	}, [movie, asPath])
+    useEffect(() => {
+        setUrl('')
+        setPlay(false)
+        setIdFile('')
+    }, [movie, asPath])
 
-	useEffect(() => {
-		if (idFile !== '') {
-			PortalMovieService.getUrl(idFile).then((data) => {
-				setUrl(data.data.url)
+    useEffect(() => {
+        if (idFile !== '') {
+            PortalMovieService.getUrl(idFile).then((data) => {
+                setUrl(data.data.url)
 
-			})
-		}
-	}, [idFile, url])
+            })
+        }
+    }, [idFile, url])
 
-	return (
-		<div className={styles.movie}>
-			{isLoading && <MovieSkeleton />}
-			{movie && (
-				<>
-					<div className={styles.main}>
-						<MovieDescription movie={movie} />
-						<div className={styles.right}>
-							<div className={styles.image}>
-								{movie.logo && (
-									<Image
-										src={movie.logo}
-										alt={movie.title}
-										width={450}
-										height={650}
-										priority
-										unoptimized
-									/>
-								)}
-							</div>
-							<Rating
-								kp={movie.rate_kp}
-								imdb={movie.rate_imdb}
-								age={movie.rate_age}
-							/>
+    return (
+        <div className={styles.movie}>
+            {isLoading && <MovieSkeleton/>}
+            {movie && (
+                <>
+                    <div className={styles.main}>
+                        <MovieDescription movie={movie}/>
+                        <div className={styles.right}>
+                            <div className={styles.image}>
+                                {movie.logo && (
+                                    <Image
+                                        src={movie.logo}
+                                        alt={movie.title}
+                                        width={450}
+                                        height={650}
+                                        priority
+                                        unoptimized
+                                    />
+                                )}
+                            </div>
+                            <Rating
+                                kp={movie.rate_kp}
+                                imdb={movie.rate_imdb}
+                                age={movie.rate_age}
+                            />
 
-							<div className='flex-center-between mt-3'>
-								<FilmTag type={movie.access} />
-								<Vote vote={movie.vote} my_vote={3} onClick={() => 5} />
-							</div>
-						</div>
-					</div>
+                            <div className={styles.votes}>
+                                <FilmTag type={movie.access}/>
+                                <Vote vote={movie.vote} my_vote={3} onClick={() => 5}/>
+                            </div>
+                        </div>
+                    </div>
 
-					<div className='p-layout mx-layout'>
-						<Tabs media={movie.media} fn={handleMovie} />
-						<VideoPLayer url={url} play={play} typeContent={movie.type_content} slug={movie.id} />
-					</div>
-				</>
-			)}
-		</div>
-	)
+                    <div className={styles.movieContainer}>
+                        <Tabs media={movie.media} fn={handleMovie}/>
+                        <VideoPLayer url={url} play={play} typeContent={movie.type_content} slug={movie.id}/>
+                    </div>
+                </>
+            )}
+        </div>
+    )
 }
 
 export default Movie
