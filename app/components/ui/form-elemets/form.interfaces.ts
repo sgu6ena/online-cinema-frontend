e6a08ae51580053b58f5fdca5355db10eec1364e
sgu6ena <issar@admin.idknet.com>
2022-05-25 11:29:20 +0300

    delete dependencies

--- app/components/ui/form-elemets/form.interfaces.ts
+++ app/components/ui/form-elemets/form.interfaces.ts
@@ -1,13 +1,13 @@
-import {ButtonHTMLAttributes, CSSProperties, InputHTMLAttributes} from 'react'
-import {EditorProps} from 'react-draft-wysiwyg'
-import {FieldError} from 'react-hook-form'
+import { ButtonHTMLAttributes, InputHTMLAttributes } from 'react'
+
+import { FieldError } from 'react-hook-form'
 
 export interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export interface IFieldProps {
-    placeholder: string
-    error?: FieldError | undefined
+	placeholder: string
+	error?: FieldError | undefined
 }
 
 type TypeInputPropsField = InputHTMLAttributes<HTMLInputElement> & IFieldProps
@@ -15,19 +15,3 @@ type TypeInputPropsField = InputHTMLAttributes<HTMLInputElement> & IFieldProps
 export interface IField extends TypeInputPropsField {
 }
 
-type TypeEditorPropsField = EditorProps & IFieldProps
-
-export interface ITextEditor extends Omit<TypeEditorPropsField, 'editorState'> {
-    onChange: (...event: any[]) => void
-    value: string
-}
-
-export interface IUploadField {
-    folder?: string
-    value?: string
-    onChange: (...event: any[]) => void
-    placeholder: string
-    error?: FieldError
-    style?: CSSProperties
-    isNoImage?: boolean
-}
