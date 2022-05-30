356f2cc5e538b3f98be5e39c0b4ca1504434e30b
sgu6ena <issar@admin.idknet.com>
2022-05-30 15:40:24 +0300

    favorites auth

--- app/components/screens/favorites/favorites.tsx
+++ app/components/screens/favorites/favorites.tsx
@@ -1,25 +1,27 @@
 import { FC } from 'react'
 
-import { LINKS } from '../../../config/links'
+
 import CatalogLoader from '../../loaders/CatalogLoader'
 import Pagination from '../../ui/Pagination'
-import styles from '../../ui/catalog-movies/Catalog.module.scss'
+
 import GaleryPortal from '../../ui/gallery/GaleryPortal'
 import Heading from '../../ui/heading/Heading'
-import AuthButton from '../../ui/videoPlayer/Placeholder/AuthButton'
+
 
 import { useBookmarks } from './useBookmarks'
+import { useRouter } from 'next/router'
+import AuthButton from './AuthButton'
 
 const Favorites: FC = () => {
 	const { movies, isLoading, pagination, isUser } = useBookmarks()
-
+	const {asPath}=useRouter()
 	return (
 		<div>
 			<Heading title={'Избранное'} />
 			{!isUser ? (
 				<div className='mt-32 text-center'>
 					<div className='mb-5 text-2xl'>Для просмотра вашего избранного вы должны войти</div>
-					<AuthButton slug={LINKS.LOGIN} />
+					<AuthButton slug={asPath} />
 				</div>
 			) : !isLoading && movies && pagination ? (
 				<>
