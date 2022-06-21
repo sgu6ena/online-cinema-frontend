7c6fae9a1ba19955ae3f820e31ab887fa62998f6
sgu6ena <issar@admin.idknet.com>
2022-06-21 11:48:09 +0300

    login to vote

--- app/components/ui/vote/Vote.tsx
+++ app/components/ui/vote/Vote.tsx
@@ -5,34 +5,35 @@ import { IVotePortal } from '../../../shared/types/movie.types'
 import MaterialIcon from '../MaterialIcon'
 
 import styles from './Vote.module.scss'
-import { log } from 'util'
+import { useAuth } from '../../../hooks/useAuth'
+import { toastError } from '../../../utils/toast-error'
+import { toast } from 'react-hot-toast'
 
 interface IVote {
-	vote: IVotePortal;
-	onClick: (props: { movieId: any; vote: number }) => void;
-	my_vote: 0 | 1 | 2 | 3;
+	vote: IVotePortal
+	onClick: (props: { movieId: any; vote: number }) => void
+	my_vote: 0 | 1 | 2 | 3
 	movieId: string
 }
 
 const Vote: FC<IVote> = ({
-													 vote: {
-														 // ats,
-														 dislike,
-														 like,
-													 },
-													 onClick,
-													 movieId,
-													 my_vote,
-												 }) => {
-
-
+	vote: {
+		// ats,
+		dislike,
+		like,
+	},
+	onClick,
+	movieId,
+	my_vote,
+}) => {
+	const {user} = useAuth()
 	return (
 		<div className={styles.vote}>
 			{console.log(my_vote)}
 			<button
 				className={cn(styles.like, my_vote === 3 && styles.myVote)}
-				onClick={() => onClick({ movieId, vote: my_vote === 3 ? 0 : 3 })}
-				title='Понравился фильм'
+				onClick={() => user ? onClick({ movieId, vote: my_vote === 3 ? 0 : 3 }) : toast.error('Вы должны войти для голосования')}
+				title="Понравился фильм"
 			>
 				<MaterialIcon name={'MdThumbUp'} />
 				{like}
@@ -40,8 +41,8 @@ const Vote: FC<IVote> = ({
 
 			<button
 				className={cn(styles.dislike, my_vote === 1 && styles.myVote)}
-				onClick={() => onClick({ movieId, vote: my_vote === 1 ? 0 : 1 })}
-				title='Не понравился фильм'
+				onClick={() =>  user ? onClick({ movieId, vote: my_vote === 1 ? 0 : 1 })  : toast.error('Вы должны войти для голосования')}
+				title="Не понравился фильм"
 			>
 				<MaterialIcon name={'MdThumbDown'} />
 				{dislike}
