373019b8be57129e518389f9733950bf84aaaf44
sgu6ena <issar@admin.idknet.com>
2022-06-27 14:48:34 +0300

    loader for movie

--- app/components/loaders/HomeLoading.tsx
+++ app/components/loaders/HomeLoading.tsx
@@ -2,34 +2,20 @@ import React from 'react'
 
 import SkeletonLoader from '../ui/SkeletonLoader'
 import styles from './loaders.module.scss'
+import { SliderMovieBlock } from './SliderMovieBlock'
 
-const Block = () => (<>
-	<div className={styles.titleContainer}>
-		<SkeletonLoader className={styles.title} />
-		<SkeletonLoader className={styles.button} />
-	</div>
-	<div className={styles.mainContainer}>
-		<SkeletonLoader className={styles.item} />
-		<SkeletonLoader className={styles.item} />
-		<SkeletonLoader className={styles.item} />
-		<SkeletonLoader className={styles.item} />
-		<SkeletonLoader className={styles.item} />
-		<SkeletonLoader className={styles.item} />
-		<SkeletonLoader className={styles.item} />
-		<SkeletonLoader className={styles.item} />
-	</div>
-</>)
+
 
 const HomeLoading = () => {
 	return (
 		<div className={'overflow-hidden'}>
 			<SkeletonLoader className={styles.mainBanner} />
 			<div className='px-layout '>
-				<Block />
-				<Block />
-				<Block />
-				<Block />
-				<Block />
+				<SliderMovieBlock />
+				<SliderMovieBlock />
+				<SliderMovieBlock />
+				<SliderMovieBlock />
+				<SliderMovieBlock />
 			</div>
 		</div>
 	)
--- app/components/loaders/MovieSkeleton.tsx
+++ app/components/loaders/MovieSkeleton.tsx
@@ -1,28 +1,39 @@
-import React, {FC} from 'react';
-import SkeletonLoader from "../ui/SkeletonLoader";
+import React, { FC } from 'react'
+import SkeletonLoader from '../ui/SkeletonLoader'
+import { SliderMovieBlock } from './SliderMovieBlock'
 
 const MovieSkeleton: FC = () => {
-    return (
-        <div className='pl-layout pt-layout flex justify-between'>
-            <div className='w-3/4 mr-5'>
-                <div className='w-3/4 '>
-                    <div className='w-60'>
-                    <SkeletonLoader count={1} className={'h-12 w-12 '}/>
-                    </div>
-                    <div className='w-full'>
-                        <SkeletonLoader count={1} className={'h-5 mt-10'}/>
-                    </div>
-                    <div className='w-96'>
-                        <SkeletonLoader count={1} className={'h-5 mt-10'}/>
-                    </div>
+	return (
+		<>
+			<div className='pr-5 pt-layout flex justify-between'>
+				<div className='w-1/3 mr-5'>
 
-                </div><SkeletonLoader count={5} className={'h-5 mt-6'}/>
-            </div>
-            <div className='w-1/4 '>
-                <SkeletonLoader count={1} className={'h-96'}/>
-            </div>
-        </div>
-    );
-};
+					<SkeletonLoader count={1} className={'h-96'} />
+					<div className='w-32 pt-3'>
+						<SkeletonLoader count={1} className={'h-12 w-12 '} />
+					</div>
+				</div>
+				<div className='w-2/3 '>
+					<div className='w-3/4 '>
+						<div className='w-60'>
+							<SkeletonLoader count={1} className={'h-12 w-12 '} />
+						</div>
+						<div className='w-full'>
+							<SkeletonLoader count={1} className={'h-5 mt-10'} />
+						</div>
+						<div className='w-96'>
+							<SkeletonLoader count={1} className={'h-5 mt-10'} />
+						</div>
+					</div>
+					<SkeletonLoader count={6} className={'h-5 mt-6'} />
+				</div>
+			</div>
+			<div className="pr-5 pt-layout">
+				<SliderMovieBlock />
+			</div>
 
-export default MovieSkeleton;
+		</>
+	)
+}
+
+export default MovieSkeleton
