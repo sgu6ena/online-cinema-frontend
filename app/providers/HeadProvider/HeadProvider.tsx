d6edde75e9d51653a2f5ef84be8c81c9f7bd1b0d
sgu6ena <sgu6ena@gmail.com>
2022-10-06 12:56:12 +0300

    test pwa

--- app/providers/HeadProvider/HeadProvider.tsx
+++ app/providers/HeadProvider/HeadProvider.tsx
@@ -16,15 +16,20 @@ const HeadProvider: FC = ({ children }) => {
 				height={4}
 			/>
 			<Head>
-				<meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+				<meta httpEquiv='Content-Type' content='text/html;charset=UTF-8' />
 				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1.0, maximum-scale=1.8"
+					name='viewport'
+					content='width=device-width, initial-scale=1.0, maximum-scale=1.8'
 				/>
 				<Favicons />
-				<meta name="theme-color" content="#18181E" />
-				<meta name="msapplication-navbutton-color" content="#18181E" />
-				<meta name="apple-mobile-web-app-status-bar-style" content="#18181E" />
+				<meta name='theme-color' content='#18181E' />
+				<meta name='full-screen' content='yes' />
+				<meta name='apple-mobile-web-app-capable' content='yes' />
+				<meta name='mobile-web-app-capable' content='yes' />
+				<meta name='apple-mobile-web-app-title' content='PORTAL.IDC.MD' />
+				<meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />
+				<meta name='msapplication-navbutton-color' content='#18181E' />
+				<meta name='apple-mobile-web-app-status-bar-style' content='#18181E' />
 			</Head>
 			{children}
 		</>
