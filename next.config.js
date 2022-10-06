cb51bcb50900647387043b57e74644ea4b2b276e
sgu6ena <sgu6ena@gmail.com>
2022-10-06 16:12:08 +0300

    "purpose": "maskable"

--- next.config.js
+++ next.config.js
@@ -1,28 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-
-}
-
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+	dest: 'public'
+})
 
 
+module.exports = module.exports = withPWA({
+	poweredByHeader: false,
+	optimizeFonts: false,
+	env: {
+		APP_URL: process.env.REACT_APP_URL,
+		APP_ENV: process.env.REACT_APP_ENV,
+		APP_SERVER_URL: process.env.REACT_SERVER_APP_URL,
+	},
+	images: {
+		domains: ['portal.idc.md'],
+		dangerouslyAllowSVG: true,
+	},
+})
 
-module.exports = async (phase, { defaultConfig }) => {
-	/**
-	 * @type {import('next').NextConfig}
-	 */
-	const nextConfig = {
-		poweredByHeader: false,
-		optimizeFonts: false,
-		env: {
-			APP_URL: process.env.REACT_APP_URL,
-			APP_ENV: process.env.REACT_APP_ENV,
-			APP_SERVER_URL: process.env.REACT_SERVER_APP_URL,
-		},
-		images: {
-			domains: ['portal.idc.md'],
-			dangerouslyAllowSVG: true,
-		},
-	}
-	return nextConfig
-}
+
