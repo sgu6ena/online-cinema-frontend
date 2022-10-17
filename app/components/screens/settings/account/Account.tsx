459fd2613faaaf134ab47ee0b50e954a4ec746af
sgu6ena <sgu6ena@gmail.com>
2022-10-17 10:24:23 +0300

    account

--- app/components/screens/settings/account/Account.tsx
+++ app/components/screens/settings/account/Account.tsx
@@ -7,6 +7,7 @@ import styles from '../settings.module.scss'
 
 import Modal from './Modals/Modal'
 import ChangePass from './Modals/ChangePass'
+import Button from '../../../ui/form-elemets/Button'
 
 const Account: FC = () => {
 	const { user, isLoading } = useAuth()
@@ -16,21 +17,49 @@ const Account: FC = () => {
 		<div>
 			<div className={'flex justify-between max-w-5xl '}>
 				<Heading title={'АККАУНТ'} className='mb-5' />
-				{/*{user && <img width={100} height={100} src={user.avatar} alt={user.login} className='rounded rounded-full' />}*/}
-			</div>
 
+			</div>
+<Button onClick={() => setShowModal(true)}>Изменить пароль</Button>
 			{isLoading && <SkeletonLoader />}
 			{user && (
 				<>
 					<table className={styles.table}>
 						<tbody>
-						<tr><td>Логин</td><td><span>{user.login}</span></td></tr>
-						<tr><td>Пароль</td><td>********</td><td align='right' onClick={() => setShowModal(true)}>Изменить пароль</td></tr>
-						<tr><td>E-mail</td><td><span>{user.email}</span></td></tr>
-						{user.name &&<tr>
-								<td>Имя</td>
-								<td><span>{user.name}</span></td>
-							</tr>}
+						<tr>
+							<td>Логин</td>
+							<td><span>{user.login}</span></td>
+						</tr>
+						<tr>
+							<td>Пароль</td>
+							<td>********</td>
+							<td align='right'></td>
+						</tr>
+						<tr>
+							<td>E-mail</td>
+							<td><span>{user.email}</span></td>
+						</tr>
+						{user.name && <tr>
+							<td>Имя</td>
+							<td><span>{user.name}</span></td>
+						</tr>}
+						<tr>
+							<td>Подписка</td>
+							<td><span>{user.subscription ? 'активна, ' : 'не активна'}{user.dtEnd}</span></td>
+						</tr>
+						{user.subscription ? <>
+							<tr>
+								<td>Автопродление</td>
+								<td><span>{user.dtFlow ? 'отключено' : 'активно'}</span></td>
+							</tr>
+							<tr>
+								<td>Точка оплаты</td>
+								<td><span>{user.point ? user.point : '-'}</span></td>
+							</tr>
+						</> : ''}
+						<tr>
+							<td>Портал за рубль</td>
+							<td><span>{user.promo ? 'доступен' : 'недоступен'}</span></td>
+						</tr>
 						</tbody>
 					</table>
 				</>
