72dd6fe88a9a69c4a996ec47ae47f14f51268e10
sgu6ena <sgu6ena@gmail.com>
2022-10-19 10:53:24 +0300

    breadcrumnbs on film

--- pages/genres/[id].tsx
+++ pages/genres/[id].tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next'
 import dynamic from 'next/dynamic'
 
 const DynamicCatalogPortal = dynamic(
-	() => import('../../app/components/screens/genreAlt/Catalog'),
+	() => import('../../app/components/screens/genre/Catalog'),
 	{
 		ssr: false,
 	}
