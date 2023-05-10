69066878bcaa3f0fb0544022dde0b6a192348498
Иссар Наталья Сергеевна <issar@callcenter.idknet.com>
2023-05-10 11:24:19 +0300

    fix link

--- app/components/ui/videoPlayer/Placeholder/ProfilePlaceholder.tsx
+++ app/components/ui/videoPlayer/Placeholder/ProfilePlaceholder.tsx
@@ -2,13 +2,14 @@ import Link from 'next/link'
 import { FC } from 'react'
 
 import styles from './AuthPlaсeholder.module.scss'
+import { LINKS } from '@/config/links'
 
 const ProfilePlaceholder: FC = () => {
 	return (
 		<div className={styles.placeholder}>
 			<div>
 				<div>Для просмотра фильма вы должны приобрести подписку</div>
-				<Link href={`/profile`}>
+				<Link href={LINKS.SUBSCRIPTIONS}>
 					<a className={styles.btn}>Личный кабинет</a>
 				</Link>
 			</div>
