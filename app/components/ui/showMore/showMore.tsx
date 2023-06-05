e80bf468b6ebdb37e2eb1c8ff4ac8ded9cb23970
Иссар Наталья Сергеевна <issar@callcenter.idknet.com>
2023-06-05 12:56:12 +0300

    fix styles

--- app/components/ui/showMore/showMore.tsx
+++ app/components/ui/showMore/showMore.tsx
@@ -13,7 +13,7 @@ const ShowMore: FC<{totalPages?:number, setPage:(page:number)=>void, page:number
 		<div className={'flex justify-center'}>
 			{totalPages && totalPages > page && (
 				<button
-					className='flex border border-primary gap-4 mt-5 pl-8 py-2 rounded-lg pr-5 items-center'
+					className='flex btn-primary gap-4 mt-5 pl-8 py-2 rounded-lg pr-5 items-center'
 					onClick={showMore}
 				>
 					Показать еще <MdKeyboardArrowDown className='h-6 w-6' />
