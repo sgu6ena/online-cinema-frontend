1d2313228423edfa8722e5d7bf487e5e785171a6
Иссар Наталья Сергеевна <issar@callcenter.idknet.com>
2023-06-20 17:18:00 +0300

    fix genres

--- app/components/screens/admin/genres/genres.tsx
+++ app/components/screens/admin/genres/genres.tsx
@@ -3,27 +3,25 @@ import { useGenres } from './useGenres'
 import AdminGenresTable from '@/ui/AdminTable/AdminTableGenres/AdminGenresTable'
 import AdminHeader from '@/ui/AdminHeader/AdminHeader'
 import { useRouter } from 'next/router'
+import { iAdminGenre } from '../../../../api/admin/admin.service'
 
 const Genres: FC = () => {
 	const { genres, isLoading } = useGenres()
 	const { push } = useRouter()
+	const [sortGenres, setSortGenres] = useState<iAdminGenre[]>([])
 	const [term, setTerm] = useState('')
-	const [sortGenres, setSortGenres] = useState(genres)
-
-	// const sortGenres = genres.sort((a: { sort: number }, b: { sort: number }) => Number(b.sort) - Number(a.sort))
-
 
 	useEffect(() => {
-		setSortGenres(genres.filter(genre => genre.name.match(term)).sort((a: { sort: number }, b: {
-			sort: number
-		}) => Number(b.sort) - Number(a.sort)))
-	}, [term])
+		setSortGenres(genres
+			.filter(genre => genre.name.toLowerCase().includes(term.toLowerCase().trim()))
+			.sort((a: { sort: number }, b: { sort: number }) => Number(b.sort) - Number(a.sort)))
+	}, [term, genres])
 
 	return (
 		<div>
 			<AdminHeader searchTerm={term} handleSearch={(e) => setTerm(e.target.value)}
 									 onClick={() => push('genres/new')} />
-
+			{isLoading && <h1>загрузка</h1>}
 			<AdminGenresTable tableItems={sortGenres} removeHandler={(e) => console.log(e)}
 												headerItems={['сортировка', 'название', 'ID']} isLoading={isLoading} />
 		</div>
