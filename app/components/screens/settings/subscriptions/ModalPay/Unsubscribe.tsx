17e93ff434b5f8a67eaa1b37cdd5eef6fb926221
Иссар Наталья Сергеевна <issar@callcenter.idknet.com>
2023-08-03 14:31:51 +0300

    fix genre hidden

--- app/components/screens/settings/subscriptions/ModalPay/Unsubscribe.tsx
+++ app/components/screens/settings/subscriptions/ModalPay/Unsubscribe.tsx
@@ -1,12 +1,12 @@
-import { FC, useEffect } from 'react'
+import React, { FC,  useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 
 import Button from '../../../../ui/form-elemets/Button'
 import Heading from '../../../../ui/heading/Heading'
 
 import styles from './modalPay.module.scss'
-import { useActions } from '../../../../../hooks/useActions'
-import { useSettings } from '../../../../../hooks/useSettings'
+import { useActions } from '@/hooks/useActions'
+import { useSettings } from '@/hooks/useSettings'
 
 const Unsubscribe: FC<{ setIsShow: (isShow: boolean) => void }> = ({ setIsShow }) => {
 	const { unsubscribe } = useActions()
@@ -18,15 +18,26 @@ const Unsubscribe: FC<{ setIsShow: (isShow: boolean) => void }> = ({ setIsShow }
 	const onSubmit = () => {
 		unsubscribe()
 	}
+
+	const onCancel = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) =>{
+		e.preventDefault()
+		setIsShow(false)
+	}
+
 	useEffect(() => {
 		isLoading ? setIsShow(true) : setIsShow(false)
 	}, [isLoading])
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
 			<div className={styles.head}>
-				<Heading title='Вы уверены?' />
+				<Heading title='Отмена подписки' />
+				<p className={'pt-4'}>Вы уверены, что хотите отменить подписку? Подписка отключится по истечении оплаченного периода</p>
 			</div>
-			<Button type='submit'>Да, отменить подписку</Button>
+			<div className={styles.footer}>
+				<Button type='submit'>Да</Button>
+				<button className={'bg-gray-700 rounded p-2 mt-3'} onClick={(e)=>onCancel(e)}>Нет</button>
+			</div>
+
 		</form>
 	)
 }
