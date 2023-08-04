9e41ee4ab612e3487aaf2b0c3423ad1703b23ce7
Иссар Наталья Сергеевна <issar@callcenter.idknet.com>
2023-08-04 12:09:38 +0300

    сериал мульт

--- app/components/ui/AdminTable/AdminTableMovie/AdminTableMovieRow.tsx
+++ app/components/ui/AdminTable/AdminTableMovie/AdminTableMovieRow.tsx
@@ -9,9 +9,31 @@ import VisibleToggleButton from '@/screens/admin/movies/visibleToggleButton'
 const AdminTableMovieRow: FC<IAdminMovieTable> = ({ tableItem: movie, removeHandler }) => {
 	return (
 		<tr className={'m-0'}>
-			<td><Link href={`movies/${String(movie.id)}`}><a className={'flex gap-4  items-center w-full'}>
-				<img className={''} src={movie.logo}
-						 alt={movie.title} />{movie.title} {movie.links > 1 ? `(${movie.links})` : ''}
+			<td><Link href={`movies/${String(movie.id)}`}><a
+				className={'flex gap-4 relative  items-center w-full'}>
+
+				<img className={movie.links > 1 ? ' border border-[#6A5ACD] z-2' : ''} src={movie.logo}
+						 alt={movie.title} />
+				{movie.links > 1
+					&& <span
+						className={'absolute bottom-[-8px] bg-[#6A5ACD] rounded text-xs text-white font-bold px-1 z-40 left-[-31px]'}>
+						сериал
+				</span>}
+				{
+					movie.genre.filter(genre=>genre.id==20).length>0	&& <span
+						className={'absolute top-[-8px] bg-[#FFFF00] rounded text-xs text-black font-bold px-1 z-40 left-[-32px]'}>
+						мульт
+				</span>
+				}
+				{
+					movie.genre.filter(genre=>genre.id==45).length>0	&& <span
+						className={'absolute top-[-8px] bg-[#008000] rounded text-xs text-white font-bold px-1 z-40 left-[64px]'}>
+						шоу
+				</span>
+				}
+
+
+			 {movie.title} {movie.links > 1 ? `(${movie.links})` : ''}
 			</a></Link>
 			</td>
 			<td>{movie.age}</td>
